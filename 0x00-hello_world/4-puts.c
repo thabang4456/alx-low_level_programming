a0d6ce76f75c3c9ea70ba6a63c35d2b96463138f
thabang <bthabang81@gmail.com>
2022-06-02 12:22:37 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+/**
+ *  * main - entry point
+ *   * @void: no arguments
+ *    * Description: the program starts and ends here.
+ *     * Return: value 0 to exit
+ *      */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
