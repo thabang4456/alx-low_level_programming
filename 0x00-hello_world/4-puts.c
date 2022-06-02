ac7234485113dd5f68e3fb80fea77859e7dce4d9
thabang <bthabang81@gmail.com>
2022-06-02 14:50:15 -0700

    #include <stdio.h> /**  * main - entry point  * @void: no arguments  * Description: the program starts and ends here.  * Return: value 0 to exit  */ int main(void) { printf("Size of a char: %d byte(s)\n", sizeof(char)); printf("Size of an int: %d byte(s)\n", sizeof(int)); printf("Size of a long int: %d byte(s)\n", sizeof(long int)); printf("Size of a long long int: %d byte(s)\n", sizeof(long long int)); printf("Size of a float: %d byte(s)\n", sizeof(float)); return (0); }

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/**
- *  * main - entry point
- *   * @void: no arguments
- *    * Description: the program starts and ends here.
- *     * Return: value 0 to exit
- *      */
+/*
+ * main - entry point
+ *@void: no arguments
+ *Description: the program starts and ends here.
+ *Return: value 0 to exit 
+ */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-                 	return (0);
+{puts("Programming is like building a multilingual puzzle");
+return (0);
 }
