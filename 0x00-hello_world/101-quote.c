5ee58885f1deefe617b68fd926933a6c8d5055f8
thabang <bthabang81@gmail.com>
2022-06-06 16:07:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-nclude <unistd.h>
+#include <unistd.h>
 
 /**
  *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -8,9 +8,9 @@ nclude <unistd.h>
  */
 int main(void)
 {
-		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
+	 write(2,
+			 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+				      	      59);
 
 			return (1);
 }
