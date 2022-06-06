c68eb868dd047ed294a325855211ae00cc307ed2
thabang <bthabang81@gmail.com>
2022-06-06 15:51:59 -0700

    its great

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *   * @void: no arguments
  *    * Description: the program starts and ends here.
  *     * Return: value 0 to exit
- *      */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
