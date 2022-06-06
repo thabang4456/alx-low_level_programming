e8e6ce848397b00950fac83b55803c3bd782ae2a
thabang <bthabang81@gmail.com>
2022-06-06 15:28:56 -0700

    its great

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzz                                                :le");
+		puts("\"Programming is like building a multilingual puzz:
+le");
 
 			return (0);
 }
+
