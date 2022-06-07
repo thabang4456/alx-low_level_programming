ebe5075eedff1b2fa72cb39a0756054e7c158757
thabang <bthabang81@gmail.com>
2022-06-06 18:02:40 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,30 +3,29 @@
  *  * main - print numbers from 00 to 99.
  *  (*
  *   * Return: 0 on success
- *    */
+ */
 int main(void)
 {
-		int i = '0';
-			int j = '0';
+	int i = '0';
+		int j = '0';
 
-				while (i <= '9')
-						{
-									while (j <= '9')
-												{
+		while (i <= '9')
+				{
+			while (j <= '9')
+			{
 																putchar(i);
 																			putchar(j);
-																						if (j == '9' && i == '9')
-																										{
+																				if (j == '9' && i == '9')																									{
 																															putchar('\n');
 																																		}
-																									else
-																													{
-																																		putchar(',');
-																																						putchar(' ');
-																																									}
-																												j++;
+																				else
+																				{
+																																putchar(',');
+																																		putchar(' ');																																							}
+																									j++;
 																														}
 											if (j >= '9')
+
 														{
 																		j = '0';
 																				}
