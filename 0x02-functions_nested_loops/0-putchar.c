b098044dccdcd031360c958121e627a36a754491
thabang <bthabang81@gmail.com>
2022-06-08 14:03:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/**
+ * main- Ap program that prints _putchar followed by
+ * a new line without using the stdio
+ *  *
+ *   * Return: 0 on success
+ *    *
+ */
+
+int  main(void)
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (0);
+
+}
